40ad0b6cdb30de8e5d02e48c24eee02a7204be51
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-28 18:05:15 -0500

    Todo routing templating and use

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+//Require Global
 const express = require('express');
 const app = express();
 const PORT = 3001;
 
+//Middleware
+const todoRoutes = require('./routes/todos');
+app.use('/api/todos', todoRoutes);
+
+//Routes
 app.get('/', (req, res) => {
-  res.send('API')
+  res.send('Landing')
 });
 
+//Listener
 app.listen(PORT, () => {
   console.log('Todo API running on port: ' + PORT);
 });
