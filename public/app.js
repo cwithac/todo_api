070c66b85863fb37c3920e2bc867e01004faff18
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-28 19:57:47 -0500

    Conditional completed css

--- public/app.js
+++ public/app.js
@@ -6,6 +6,9 @@ $(document).ready(() => {
 const addTodos = (todos) => {
   todos.forEach(todo => {
     const newTodo = $('<li>' + todo.name + '</li>').addClass('task');
+    if (todo.completed) {
+      newTodo.addClass('done');
+    }
     $('.list').append(newTodo);
   })
 };
