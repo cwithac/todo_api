6f502c449dd571d7cd09404aa06258cd9c76ed37
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-06 18:53:05 -0500

    Heroku CORS

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const todoRoutes = require('./routes/todos');
 app.use('/api/todos', todoRoutes);
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 //Routes
 app.get('/', (req, res) => {
